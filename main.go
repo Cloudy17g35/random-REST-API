030c31cab5d3c0fcda84677f1bfea65f69e56ef6
Cloudy17g35 <ksawery.lejczak@gmail.com>
2022-07-15 10:42:32 +0200

    adding main.go

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"time"
+	"random-api/httpd/handlers"
+	"github.com/gin-contrib/timeout"
+	"github.com/gin-gonic/gin"
+)
+
+const default_timeout = 10 * time.Second
+
+func main() {
+	engine := gin.New()
+	engine.GET(
+	"random/mean",
+	timeout.New(
+	timeout.WithTimeout(default_timeout),
+    timeout.WithHandler(handlers.RandomMeanHandler)),
+	)
+	
+
+	engine.Run()
+}
