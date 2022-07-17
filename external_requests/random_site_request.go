9946e3effbd1aecd361cff69cd627dde83c17435
Cloudy17g35 <ksawery.lejczak@gmail.com>
2022-07-17 18:03:57 +0200

    chaning fatal logs to logs from logger/logger

--- external_requests/random_site_request.go
+++ external_requests/random_site_request.go
@@ -1,11 +1,12 @@
 package external_requests
 
 import (
-	"log"
 	"net/http"
 	"time"
 	"io/ioutil"
+	"random-api/logger"
 )
+
 
 const RandomURL = "https://www.random.org/integers/?min=1&max=100&col=1&base=10&format=plain&rnd=new"
 
@@ -14,22 +15,29 @@ func GetRandomNumbers(length string) (int, string, error) {
 	client := &http.Client{
 		Timeout: 5 * time.Second,
 	}
-	req, err := http.NewRequest(http.MethodGet, RandomURL, nil)
+	logs := logger.GetLogger()
+	req, _ := http.NewRequest(http.MethodGet, RandomURL, nil)
 	var empty_res string
 	q := req.URL.Query()
 	q.Add("num", length)
 	req.URL.RawQuery = q.Encode()
 	resp, err := client.Do(req,)
 	if err != nil {
-		log.Fatal(err)
+		format := "error occured while connecting to external service: %v"
+		logs.Errorf(
+			format,
+			err)
 		return 0,empty_res,err
 	}
 	
 	defer resp.Body.Close()
 	responseBody, err := ioutil.ReadAll(resp.Body)
 
-	if err != nil{
-		log.Fatal(err)
+	if err != nil {
+		format := "error occured while connecting to external service: %v"
+		logs.Errorf(
+			format,
+			err)
 		return 0,empty_res,err
 	}
 
